e66837cbdba9c55519840950d79326382344ca0b
captainq25@gmail.com <captainq25@gmail.com@9a077753-6125-29b5-898e-54377ac2f33f>
2013-02-07 03:18:16 +0000

    nothing

--- JoystickBot/JoystickBot.java
+++ JoystickBot/JoystickBot.java
@@ -4,7 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
+//comment
 package edu.wpi.first.wpilibj.templates;
 
 
