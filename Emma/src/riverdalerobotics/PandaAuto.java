63055d4389ea95a8888a135cdb4ce7e51caf6f65
mavbotcode@gmail.com <mavbotcode@gmail.com@9a077753-6125-29b5-898e-54377ac2f33f>
2013-02-08 03:41:21 +0000

    Adding step methods and cleaning up PandaBot.java

---
+++ Emma/src/riverdalerobotics/PandaAuto.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package riverdalerobotics;
+
+/**
+ *
+ * @author FIRST
+ */
+public class PandaAuto {
+    private PandaDrive train;
+    private PandaShoot shooter;
+    private PandaLift lifter;
+    
+    PandaAuto(PandaDrive train, PandaShoot shooter, PandaLift lifter){
+	this.train = train;
+	this.shooter = shooter;
+	this.lifter = lifter;
+    }
+    
+    public void step() {
+	//do autonomous stuff
+    }
+    
+}
