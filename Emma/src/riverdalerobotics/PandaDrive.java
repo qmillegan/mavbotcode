6331c8c4a426fa50e0e02762272cbb1b97b89ace
captainq25@gmail.com <captainq25@gmail.com@9a077753-6125-29b5-898e-54377ac2f33f>
2013-02-09 21:06:48 +0000

    changes to pada drive, trying to prevent flipping

--- Emma/src/riverdalerobotics/PandaDrive.java
+++ Emma/src/riverdalerobotics/PandaDrive.java
@@ -5,20 +5,31 @@ package riverdalerobotics;
 
 /**
  *
- * @author FIRST
+ * 
  */
 public class PandaDrive {
 
     private Joystick joystick;
     private Jaguar jagLeft, jagRight;
-    
+ 
     public PandaDrive(Jaguar jagLeft, Jaguar jagRight, Joystick joystick){
         this.joystick = joystick;
         this.jagLeft = jagLeft;
         this.jagRight = jagRight;
     }
     public void drive(){
-        double speed = joystick.getY();
+        final double maxAccel = 5;
+        double speed = joystick.getY(); 
+        double time = timer;
+        double lastSpeed = speed;
+        double lastTime = time;
+        double timeDif = time-lastTime;
+        double speedDif = speed-lastSpeed;
+        double accel = speedDif/timeDif;
+        if(Math.abs(accel) > maxAccel){
+            double newSpeed = maxAccel*timeDif;
+            speed = newSpeed;
+        } 
         jagRight.set(speed);
         jagLeft.set(speed);
 }
