ef5eebb9127e505e3ad9e0b623b3dde41c976000
captainq25@gmail.com <captainq25@gmail.com@9a077753-6125-29b5-898e-54377ac2f33f>
2013-02-20 04:03:21 +0000

    made autonomous mode

--- PandaBot/src/riverdale/robot/PandaAuto.java
+++ PandaBot/src/riverdale/robot/PandaAuto.java
@@ -1,5 +1,7 @@
 package riverdale.robot;
 
+import edu.wpi.first.wpilibj.*;
+
 /*
  * @author Quinn
  */
@@ -8,19 +10,42 @@ public class PandaAuto {
     private PandaDrive train;
     private PandaShoot shooter;
     private PandaLift lifter;
+    private Jaguar rotaryJag;
+    private Jaguar hopperJag;
+    private Timer timer = new Timer();
+    double sEndTime = 0;
+    double sEndTime2= 0;
+    double num=0;
+    double delayTime=0.3;
     
-    PandaAuto(PandaDrive train, PandaShoot shooter, PandaLift lifter){
+    PandaAuto(PandaDrive train, PandaShoot shooter, PandaLift lifter,Jaguar rotaryJag, Jaguar hopperJag){
 	this.train = train;
 	this.shooter = shooter;
 	this.lifter = lifter;
+        this.rotaryJag = rotaryJag;
+	this.hopperJag = hopperJag;
     }
     public void step(){
 	//do autonomous stuff 
-	//QUINN's
-        double num = 0;
-        while (num != 4) {
-            shooter.step();
-	    //adding 1 to the number,until it fires four frisbees
-            num = num + 1;
+        sEndTime = timer.get() + 0.6;
+        //starting rotary jag
+        rotaryJag.set(1);
+        if(timer.get() > sEndTime){
+            //starting hopper after sEndTime has passed
+            sEndTime = timer.get() + delayTime;
+            shooter.shoot();
+            if(timer.get() > sEndTime){
+                sEndTime = timer.get() + delayTime;
+                shooter.shoot();
+                    if(timer.get() > sEndTime){
+                        sEndTime = timer.get() + delayTime;
+                        shooter.shoot();
+                            if(timer.get() > sEndTime){
+                                sEndTime = timer.get() + delayTime;
+                                shooter.shoot();
+                                rotaryJag.set(0);
+            }
+            }
+            }
     }
 }}
