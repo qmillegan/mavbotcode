ce0e7d4bfdfc3efc2169eaa84d4c577ec3e182fa
mavbotcode <mavbotcode@9a077753-6125-29b5-898e-54377ac2f33f>
2013-02-20 04:19:44 +0000

    requires a web server. uses css to superimpose a crosshair on the video stream

---
+++ camera/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+ <title>Fire the Reener Cannon</title>
+</head>
+<body>
+<h2>2915 - IP Camera</h2>
+<div id="crosshairs" style="position: fixed; left: 385px; top: 340px;"><img src="x.png" /></div>
+<iframe style="width: 100%; height: 100%" src="http://10.29.15.11/mjpg/video.mjpg">No iframes, no show.</iframe>
+
+</body>
+</html>
